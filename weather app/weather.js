44926968e682151aa11b094b0a33ea7dbf19309d
Gargi Sharma <157142478+gargi63@users.noreply.github.com>
2024-07-12 17:17:38 +0530

    Add files via upload

---
+++ weather app/weather.js
@@ -0,0 +1,56 @@
+
+     const apikey="5561d4efb318118ac716a6c0a3d21197";
+     const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+     const searchbox=document.querySelector(".search input");
+     const searchbtn=document.querySelector(".search button");
+     const weathericon=document.querySelector(".weather-icon");
+
+     async function checkweather(city){
+        const response=await fetch(apiUrl+city+`&appid=${apikey}`);
+        if(response.status==404){
+            document.querySelector(".error").style.display="block";
+            document.querySelector(".weather").style.display="none";
+        }
+        else{
+            var data=await response.json();
+        console.log(data);
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"℃";
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+      if(data.weather[0].main=="Clouds"){
+        weathericon.src="images/clouds.png";
+
+      }
+      else if(data.weather[0].main=="Clear"){
+        weathericon.src="images/clear.png";
+
+      }
+      else if(data.weather[0].main=="Drizzle"){
+        weathericon.src="images/drizzle.png";
+
+      }
+      else if(data.weather[0].main=="Mist"){
+        weathericon.src="images/mist.png";
+
+      }
+      else if(data.weather[0].main=="Rain"){
+        weathericon.src="images/rain.png";
+
+      }
+      document.querySelector(".weather").style.display="block";
+      document.querySelector(".error").style.display="none";
+      
+    }
+
+        
+     }
+     searchbtn.addEventListener("click", () => {
+            const city = searchbox.value;
+            if (city) {
+                checkweather(city);
+            } else {
+                alert("Please enter a city name");
+            }
+        });
+    
